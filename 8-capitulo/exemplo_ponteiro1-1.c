ac40e09bbe57f6077b70bc3b5e0a56badd5ddc57
carlinhoshk <carlosmdohk@gmail.com>
2024-01-26 16:52:14 -0300

    Novo: Adicionando exec capitlo 8

---
+++ 8-capitulo/exemplo_ponteiro1-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	printf("O valor de x eh: %d \n", valor);
+	printf("O endereço de x eh: %p \n", &valor);
+	printf("O valor de pont eh: %p \n", pont);
+	printf("O edreço de pont eh: %p \n", &pont);
+	
+
+}
---
+++ 8-capitulo/exemplo_ponteiro1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+	int x = 8;
+	int *p = &x;
+	printf("Valor de x = %d \n", x);
+	printf("Valor de x acessado pelo ponteiro = %d \n", *p);
+	printf("Valor de p = %p \n", p);
+
+
+
+
+
+}
